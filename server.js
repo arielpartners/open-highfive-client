eeb78da6944602a520ffedf9f7e0918061a78c0b
jeff.ardilio <Cynergy2007>
2016-08-27 19:24:11 -0400

    proxy server for api
    Setup proxy server for resolving local ./api/** calls to remote destination. Remote destination is based on runtime configuration as environment variables.

--- server.js
+++ server.js
@@ -6,18 +6,28 @@ let express = require('express'),
     http = require('http'),
     url = require('url'),
     serveStatic = require('serve-static'),
-    path = require('path');
+    path = require('path'),
+    app = express(),
+    proxyDomain = process.env.HIGHFIVE_API_SERVER || 'arielflash-server.azurewebsites.net';
 
-let app = express();
-
-process.argv.forEach(arg => {
-    if (arg && arg.indexOf('port=') === 0) {
-        port = parseInt(arg.split('=')[1]);
-    }
-});
-
-app.use('/api', proxy(process.env.HIGHFIVE_API_SERVER || 'http://arielflash-server.azurewebsites.net/', {
-    forwardPath: function(req) {
+//proxy /api locally to the designated endpoint that should be passed in as an environment variable
+app.use('/api', proxy(proxyDomain, {
+    decorateRequest: (proxyReq, originalReq) => {
+        //FIXME: if we allow gzip compress, IIS in Azure sitting in front of Node is blowing up on a 502
+        //I think this needs a fix to web.config to handle this, may need a web.config here to override
+        delete proxyReq.headers['accept-encoding'];
+        return proxyReq;
+    },
+    intercept: (rsp, data, req, res, callback) => {
+        if (res._headers['set-cookie']) {
+            //fix any set cookie headers specific to the proxied domain back to the local domain
+            let localDomain = req.headers.host.substr(0, req.headers.host.indexOf(':') || req.headers.length);
+            res._headers['set-cookie'] = JSON.parse(JSON.stringify(res._headers['set-cookie']).replace(proxyDomain, localDomain));
+        }
+        callback(null, data);
+    },
+    forwardPath: (req) => {
+        //forward to proxy with same url including the prefix
         return `/api${url.parse(req.url).path}`;
     }
 }));
@@ -28,8 +38,5 @@ app.use(serveStatic('dist'));
 
 let server = http.createServer(app);
 server.listen(app.get('port'), () => {
-    let host = server.address().address,
-        port = server.address().port;
-
-    console.log('Server listening at http://' + host + ':' + port);
+    console.log(`Server running on port ${app.get('port')}`);
 });
