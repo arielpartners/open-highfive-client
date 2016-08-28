f75c7bc7854866f8686e19044d20d2e324dbadb0
jeff.ardilio <Cynergy2007>
2016-08-28 16:33:03 -0400

    fallback to resolve from public dir if request not found (ie fav icon)

--- server.js
+++ server.js
@@ -39,7 +39,8 @@ app.use('/api', proxy(proxyDomain, {
 
 app.set('port', process.env.PORT || packageJson.config.clientPort);
 app.use(compression());
-app.use(serveStatic('dist'));
+app.use(serveStatic('dist'));//compiled
+app.use(serveStatic('public'));//raw fallback
 app.get('*', (request, response, next) => {
     if (request.method === 'GET') {
         response.sendFile(path.join(__dirname, 'dist/index.html'));
