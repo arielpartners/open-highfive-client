6eb1c5087b91ac063aa7d6443acc1c69a44a1e1c
jeff.ardilio <Cynergy2007>
2016-08-28 16:04:34 -0400

    new dynamic configuration options
    There is a new export called "config" which has default options that are then overriden by environment and targets. Targets are used by command line arguments thrown at any command (ie npm start --mock).
    
    * `npm start` - By default, it will try to auto discover based on environment variables and enabling the proxy and mocks. You can white list mocks for endpoints that are not yet implemented and let all other implemented endpoints fall through to API proxy against the target server.
    * `npm start --development` - This will specifically target the development server endpoint via the proxy and disable all mocks.
    * `npm start --mock` - This will specifically target the json mock server and disable the remote server proxy.

---
+++ config.js
@@ -0,0 +1,49 @@
+const CONFIG = {
+    proxyDomain: process.env.HIGHFIVE_API_SERVER || 'arielflash-server.azurewebsites.net',
+    hostPort: process.env.PORT || '2368',
+    enableMocks: true,
+    enableProxy: true
+};
+
+const ENV_CONFIGS = {
+    production: {
+
+    },
+    development: {
+
+    }
+};
+
+const TARGET_CONFIG = {
+    production: {
+
+    },
+    development: {
+        proxyDomain: 'arielflash-server.azurewebsites.net',
+        enableMocks: false,
+        enableProxy: true
+    },
+    qa: {
+
+    },
+    uat: {
+
+    },
+    mock: {
+        enableMocks: true,
+        enableProxy: false
+    }
+};
+
+//assign overrides based on environment
+Object.assign(CONFIG, ENV_CONFIGS[process.env.NODE_ENV || 'development']);
+
+//assign overrides based on command line targets (ie --mock)
+process.argv.forEach(arg => {
+    arg = arg.replace('--', '');
+    if (TARGET_CONFIG.hasOwnProperty(arg)) Object.assign(CONFIG, TARGET_CONFIG[arg]);
+});
+
+console.log(JSON.stringify(CONFIG));
+
+module.exports = CONFIG;
