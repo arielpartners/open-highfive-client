3b3db7c7953d1cc242296a992d1ed92e44981d6d
Charles Fulnecky <charles.fulnecky@hbo.com>
2016-08-28 07:48:44 -0400

    Move configuration for api host into package.json

--- webpack.config.js
+++ webpack.config.js
@@ -346,8 +346,8 @@ module.exports = (function makeWebpackConfig() {
         historyApiFallback: true,
         stats: 'minimal', // none (or false), errors-only, minimal, normal (or true) and verbose
         proxy: [
-            {path: npmConfig.baseURL + '*', target:'http://localhost:' + npmConfig.apiPort},
-            {path:'/api/login', target:'http://localhost:' + npmConfig.apiPort + '/login'}
+            {path: npmConfig.baseURL + '*', target:npmConfig.apiHost + npmConfig.apiPort},
+            {path:'/api/login', target:npmConfig.apiHost + npmConfig.apiPort + '/login'}
         ]
     };
 
