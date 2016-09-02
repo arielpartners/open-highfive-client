2b4d415951986b7336afd12ae131cbf98add8271
Charles Fulnecky <charles.fulnecky@hbo.com>
2016-09-02 18:49:36 -0400

    Improve login message.

--- src/app/reducers/error/index.js
+++ src/app/reducers/error/index.js
@@ -1,13 +1,14 @@
 import {LOGIN_ERROR} from '../../action-types';
 
+const LOGIN_ERROR_MSG = 'You provided an invalid email or password, please try again.';
 //-------------------------------------------------------------------
 // Error STORE
 //-------------------------------------------------------------------
-export const error = (state = null, {type, payload}) => {
+export const error = (state = null, {type}) => {
     /* eslint-disable indent */
     switch (type) {
         case LOGIN_ERROR:
-            return payload;
+            return LOGIN_ERROR_MSG;
 
         default:
             return null;
