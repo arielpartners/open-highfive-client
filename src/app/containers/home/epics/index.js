12803a04aa0d9f3af158003207b9ddaf3ae561ff
Alan Fernandez <Alanf718@gmail.com>
2016-09-03 17:23:41 -0400

    fixed annoying bug

--- src/app/containers/home/epics/index.js
+++ src/app/containers/home/epics/index.js
@@ -7,21 +7,12 @@ import * as ActionTypes from '../../../action-types';
 import * as Actions from '../actions';
 
 const BASE_URL = '/api/recognitions';
-// const CHANGE_USER_URL = '/users';
 const HEADER = {'Content-Type': 'application/json'};
 
-// const spawnEpic = (epicFactory, ...actions) => {
-//     const input$ = Observable.of(...actions);
-//     const actions$ = new ActionsObservable(input$);
-//     return epicFactory(actions$);
-// };
-
 const getRecentRecognitionEpic = action$ =>
-    // action$.ofType(ActionTypes.LOGIN_PENDING)
     action$.ofType(ActionTypes.REQUEST_RECOGNITIONS)
         .mergeMap(action =>
             ajax.post(BASE_URL, JSON.stringify(action.payload), HEADER)
-                .map(Actions.getRecentRecognition)
                 .catch(error => Observable.of({type: ActionTypes.RECOGNITIONS_ERROR, payload: error}))
         );
 
