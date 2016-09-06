1a85f7878c5d21f419579e106d8d6d11323f1911
alanf718 <alanf718@gmail.com>
2016-09-06 11:55:30 -0400

    updated styles and colors for new categorys on metrics and recognition cards

--- src/app/containers/home/components/recognize-peer/index.jsx
+++ src/app/containers/home/components/recognize-peer/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {getFormData} from '../../../../index';
 
 /* istanbul ignore next */
@@ -21,104 +21,159 @@ const onSubmit = (form, getData, inputs, createRecognition, statics) => {
     }
 };
 /* eslint-enable max-params */
+/*
+ <script>
+ $(document).ready(function(){
+ $('[data-toggle="popover"]').popover();
+ });
+ </script>
+ */
 
-export const RecognizePeer = ({users, createRecognition}) => {
-    let form,
-        statics = [
-            {senderEmail : 'test@test.com'},
-            {corporateValueName: 'Integrity'},
-            {organizationName: 'Ariel Partners'},
-            {points: 1}
-        ],
-        inputs = [],
-        userList = users || [],
-        addInput = (input) => {
-            inputs.push(input);
-        };
+/* global $ */
+/* eslint-disable max-len */
+export class RecognizePeer extends Component {
 
-    return (
-        <form onSubmit={(event) => event.preventDefault() &
-                       onSubmit(form, getFormData, inputs, createRecognition, statics)}
-              noValidate
-              ref={(ref)=> {
-                  form = ref;
-              }}
-              className="col-lg-3 recognize-peer">
+    constructor(props) {
+        super(props);
+    }
 
-            <h2 className="h5-sectionhdr">Reward Someone <em>for a deed well done</em></h2>
+    componentDidMount() {
+        /* eslint-disable no-undef */
+        componentHandler.upgradeDom();
+        /* eslint-enable no-undef */
+    }
 
-            <div className="form-group h5-margintop20">
-                <select name="receiverEmail" className="form-control" required ref={addInput}>
-                    <option value="" disabled selected hidden>Select an Employee</option>
-                    {
-                        userList.map(
-                            (user)=>(
-                                <option
-                                    value={user.email} key={user.email}>{user.firstName + ' ' + user.lastName}</option>
+    componentDidUpdate() {
+        /* eslint-disable no-undef */
+        componentHandler.upgradeDom();
+        /* eslint-enable no-undef */
+    }
+
+    componentDidMount() {
+        /* eslint-disable no-undef */
+        componentHandler.upgradeDom();
+        /* eslint-enable no-undef */
+        $('[data-toggle="popover"]').popover();
+    }
+
+    componentDidUpdate() {
+        /* eslint-disable no-undef */
+        componentHandler.upgradeDom();
+        /* eslint-enable no-undef */
+        $('[data-toggle="popover"]').popover();
+    }
+
+    render() {
+        let {users, createRecognition} = this.props;
+
+        let form,
+            statics = [
+                {senderEmail : 'test@test.com'},
+                {organizationName: 'DHS'},
+                {points: 1}
+            ],
+            inputs = [],
+            userList = users || [],
+            addInput = (input) => {
+                inputs.push(input);
+            };
+
+        return (
+            <form onSubmit={(event) => event.preventDefault() &
+            onSubmit(form, getFormData, inputs, createRecognition, statics)}
+                  noValidate
+                  ref={(ref)=> {
+                      form = ref;
+                  }}
+                  className="col-lg-3 recognize-peer">
+
+                <h2 className="h5-sectionhdr">
+                    <a href="#" data-toggle="popover" data-trigger="hover" data-placement="left" title=""
+                       data-content="Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi."
+                       data-original-title="How to Give a Point">
+                        <span className="glyphicon glyphicon-question-sign" style={{fontSize: '70%'}}></span>
+                    </a> Give a Point
+                </h2>
+
+                <div className="form-group h5-margintop20">
+                    <select name="receiverEmail" className="form-control" required ref={addInput}>
+                        <option value="" disabled selected hidden>Select an Employee</option>
+                        {
+                            userList.map(
+                                (user)=>(
+                                    <option
+                                        value={user.email} key={user.email}>{user.firstName + ' ' + user.lastName}</option>
+                                )
                             )
-                        )
-                    }
-                </select>
-            </div>
+                        }
+                    </select>
+                </div>
 
-            <div className="form-group">
+                <div className="form-group">
                 <textarea ref={addInput}
                           className="form-control"
                           required
                           placeholder="Write a few words about what they did or why it was memorable" id="occurrence"
                           name="description"></textarea>
-            </div>
+                </div>
+                <div className="form-group">
+                    <select name="corporateValueName" className="form-control" required ref={addInput}>
+                        <option value="" disabled selected hidden>Select a Category</option>
+                        <option value="Teamwork">Teamwork</option>
+                        <option value="Improvement">Improvement</option>
+                        <option value="Delivery">Delivery</option>
+                        <option value="Experiment">Experiment</option>
+                    </select>
+                </div>
 
-            {/*<fieldset className="form-group">*/}
+                { /* Commenting out because we might go back to this later */ }
+                {/*<fieldset className="form-group" name="corporateValueName">*/}
                 {/*<label>Select a Core Value that was demonstrated:</label>*/}
                 {/*<div className="radio">*/}
-                    {/*<label>*/}
-                        {/*<input type="radio" name="corpvalue" id="corpvalueExcellence" value="Integrity"/>*/}
-                        {/*<strong>Integrity</strong>: Service Before Sell*/}
-                    {/*</label>*/}
+                {/*<label>*/}
+                {/*<input type="radio" name="corpvalue" id="corpvalueExcellence" value="Integrity"/>*/}
+                {/*<strong>Teamwork</strong>: Support My Team Members*/}
+                {/*</label>*/}
                 {/*</div>*/}
                 {/*<div className="radio">*/}
-                    {/*<label>*/}
-                        {/*<input type="radio" name="corpvalue" id="corpvalueAccountability" value="Vigilance"/>*/}
-                        {/*<strong>Vigilance</strong>: Guarding America*/}
-                    {/*</label>*/}
+                {/*<label>*/}
+                {/*<input type="radio" name="corpvalue" id="corpvalueAccountability" value="Vigilance"/>*/}
+                {/*<strong>Improvement</strong>: Streamline My Organization*/}
+                {/*</label>*/}
                 {/*</div>*/}
-                {/*<div className="radio disabled">*/}
-                    {/*<label>*/}
-                        {/*<input type="radio" name="corpvalue" id="corpvalueInitiative" value="Respect"/>*/}
-                        {/*<strong>Respect</strong>: Honoring our Partners*/}
-                    {/*</label>*/}
+                {/*<div className="radio">*/}
+                {/*<label>*/}
+                {/*<input type="radio" name="corpvalue" id="corpvalueInitiative" value="Respect"/>*/}
+                {/*<strong>Delivery</strong>: Continuous Product Delivery*/}
+                {/*</label>*/}
                 {/*</div>*/}
-                {/*<div className="radio disabled">*/}
-                    {/*<label>*/}
-                        {/*<input type="radio" name="corpvalue" id="corpvalueTeamwork" value="Excellence"/>*/}
-                        {/*<strong>Excellence</strong>: Exceeding Expectations*/}
-                    {/*</label>*/}
+                {/*<div className="radio">*/}
+                {/*<label>*/}
+                {/*<input type="radio" name="corpvalue" id="corpvalueTeamwork" value="Excellence"/>*/}
+                {/*<strong>Experiment</strong>: Open to New Ideas*/}
+                {/*</label>*/}
                 {/*</div>*/}
-                {/*<div className="radio disabled">*/}
-                    {/*<label>*/}
-                        {/*<input type="radio" name="corpvalue" id="corpvalueEmpowerment" value="Accountability"/>*/}
-                        {/*<strong>Accountability</strong>: Taking Ownership*/}
-                    {/*</label>*/}
-                {/*</div>*/}
-            {/*</fieldset>*/}
+                {/*</fieldset>*/}
 
-            {/*<div className="form-group">*/}
+                {/*<div className="form-group">*/}
                 {/*<select name="points" className="form-control" required ref={addInput}>*/}
-                    {/*<option value="" disabled selected hidden>Select a Recognition Level</option>*/}
-                    {/*<option value="50">50</option>*/}
-                    {/*<option value="40">40</option>*/}
-                    {/*<option value="30">30</option>*/}
-                    {/*<option value="20">20</option>*/}
-                    {/*<option value="10">10</option>*/}
+                {/*<option value="" disabled selected hidden>Select a Recognition Level</option>*/}
+                {/*<option value="50">50</option>*/}
+                {/*<option value="40">40</option>*/}
+                {/*<option value="30">30</option>*/}
+                {/*<option value="20">20</option>*/}
+                {/*<option value="10">10</option>*/}
                 {/*</select>*/}
                 {/*<p>You have <strong>70 points</strong> left to award this month</p>*/}
-            {/*</div>*/}
+                {/*</div>*/}
 
-            <div className="form-group">
-                <input type="submit" className="btn btn-primary" value="Give your Reward" data-dismiss="modal"/>
-                <input type="reset" className="btn btn-danger" value="Start Over" data-dismiss="modal"/>
-            </div>
-        </form>
-    );
-};
+                <div className="form-group">
+                    <input type="submit" className="btn btn-primary" value="Give your Reward" data-dismiss="modal"/>
+                    &nbsp;
+                    <input type="reset" className="btn btn-danger" value="Start Over" data-dismiss="modal"/>
+                </div>
+            </form>
+        );
+    }
+}
+/* eslint-enable max-len */
